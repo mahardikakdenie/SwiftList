e53be5d6f5f1fd0935f915c2f41aeabcdf83bc84
MahardiikaKD <dikamahar884@gmail.com>
2023-08-19 11:17:50 +0700

    Initial Commit

---
+++ SwiftListUi/SwiftListUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftListUiApp.swift
+//  SwiftListUi
+//
+//  Created by Mahardika Kessuma Denie on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftListUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
